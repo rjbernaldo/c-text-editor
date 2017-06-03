18fce5acb82394d5a17664ffa5f70432cbffbed1
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-03 16:07:03 +0800

    Add read timeout

--- c-text-editor.c
+++ c-text-editor.c
@@ -18,8 +18,10 @@ void enableRawMode() {
 
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
-  raw.c_cflag &= ~(ICRNL | IXON);
+  raw.c_cflag &= ~(CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -27,14 +29,18 @@ void enableRawMode() {
 int main() {
   enableRawMode();
 
-  char c;
+  while (1) {
+    char c;
 
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    read(STDIN_FILENO, &c, 1);
+
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     } else {
       printf("%d ('%c')\r\n", c, c);
     }
+
+    if (c == 'q') break;
   };
 
   return 0;
