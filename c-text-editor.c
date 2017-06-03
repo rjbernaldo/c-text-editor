e28072abf77c7469594e91eeb798ecc37f10401b
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-03 18:23:24 +0800

    Draw cursor

--- c-text-editor.c
+++ c-text-editor.c
@@ -12,6 +12,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 struct editorConfig {
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios;
@@ -162,7 +163,10 @@ void editorRefreshScreen() {
 
   editorDrawRows(&ab);
 
-  abAppend(&ab, "\x1b[H", 3);
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
   abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
@@ -170,6 +174,9 @@ void editorRefreshScreen() {
 }
 
 void initEditor() {
+  E.cx = 0;
+  E.cy = 0;
+
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
