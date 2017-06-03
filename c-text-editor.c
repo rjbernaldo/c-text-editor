476a56347f23bf39e9964acaee0c7b072812b91f
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-03 17:02:04 +0800

    Refactor input

--- c-text-editor.c
+++ c-text-editor.c
@@ -34,21 +34,35 @@ void enableRawMode() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
+char editorReadKey() {
+  int nread;
+  char c;
+
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+
+  return c;
+}
+
+void editorProcessKeypress() {
+  char c = editorReadKey();
+
+  switch (c) {
+    case CTRL_KEY('q'):
+      exit(0);
+      break;
+
+    default:
+      printf("%d ('%c')\r\n", c, c);
+  }
+}
+
 int main() {
   enableRawMode();
 
   while (1) {
-    char c;
-
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-
-    if (c == CTRL_KEY('q')) break;
+    editorProcessKeypress();
   };
 
   return 0;
