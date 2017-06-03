5934d0b66cff7f62cbe609b76d2f4b33df13a9bc
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-03 15:50:13 +0800

    Disable raw mode on exit

--- c-text-editor.c
+++ c-text-editor.c
@@ -1,10 +1,21 @@
+#include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
 
+struct termios orig_termios;
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode() {
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
-  raw.c_lflag &= ~(ECHO);
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+
+  raw.c_lflag &= ~(ECHO); // flip bits to hide output
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -12,6 +23,7 @@ int main() {
   enableRawMode();
 
   char c;
+
   while (read(STDIN_FILENO, &c, 1) == 1);
   return 0;
 }
