5774ad30ed24244b83d70a7d4be3f0542d57f237
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-03 16:55:39 +0800

    Add ctrl+q command to quit

--- c-text-editor.c
+++ c-text-editor.c
@@ -5,6 +5,8 @@
 #include <termios.h>
 #include <errno.h>
 
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 struct termios orig_termios;
 
 void die(const char *s) {
@@ -46,7 +48,7 @@ int main() {
       printf("%d ('%c')\r\n", c, c);
     }
 
-    if (c == 'q') break;
+    if (c == CTRL_KEY('q')) break;
   };
 
   return 0;
